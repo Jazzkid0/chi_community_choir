c662353c91de33f2f5e4ded937849c201ae78930
Jazzkid0 <28173360+Jazzkid0@users.noreply.github.com>
2024-10-30 01:54:34 +0000

    basic site bones

--- src/controllers/home.rs
+++ src/controllers/home.rs
@@ -1,9 +1,17 @@
-use axum::debug_handler;
+#![allow(clippy::missing_errors_doc)]
+#![allow(clippy::unnecessary_struct_initialization)]
+#![allow(clippy::unused_async)]
 use loco_rs::prelude::*;
+use axum::debug_handler;
+
+use crate::views;
 
 #[debug_handler]
-async fn home() -> Result<Response> {
-    format::json(())
+async fn home(
+    ViewEngine(v): ViewEngine<TeraView>,
+    // Context(ctx): State<AppContext>,
+) -> Result<Response> {
+    views::home::home(&v)
 }
 
 pub fn routes() -> Routes {
